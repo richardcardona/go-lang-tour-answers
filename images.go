01ddee78c14282ee4cc640b567ca548e30a1c688
richardcardona <richard@cardona.us>
2013-12-22 21:45:08 -0600

    initial

---
+++ images.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "code.google.com/p/go-tour/pic"
+    "image"
+    "image/color"
+)
+
+type Image struct{}
+
+func (i Image) ColorModel() color.Model {
+    return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+    return image.Rect(0, 0, 188, 188)
+}
+
+func (i Image) At(x, y int) color.Color {
+    v := uint8(x^y)
+    return color.RGBA{v, v, 255, 255}
+}
+
+func main() {
+    m := Image{}
+    pic.ShowImage(m)
+}
